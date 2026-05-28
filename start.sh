e6ebe313e6d25497d948cbaf17a4a9f6308b65a3
Rafif Phratama <rafif.phratama28@gmail.com>
2026-05-28 17:46:07 +0700

    updated requirements.txt and start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# Start FastAPI backend in the background
-uvicorn api.main:app --host 127.0.0.1 --port 8000 --workers 1 &
 
-# Wait a few seconds for the API to boot
+# Start FastAPI backend in the background (&)
+uvicorn api.main:app --host 0.0.0.0 --port 8000 &
+
+# Wait a few seconds for the API to boot up securely
 sleep 3
 
 # Start the aiogram Bot in the foreground
